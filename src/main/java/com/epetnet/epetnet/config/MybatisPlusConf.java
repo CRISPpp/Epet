df2255a0856bf1b708e33fc6c3b00effffabfd10
crisp <2715554797@qq.com>
2022-07-14 16:59:17 +0800

    update

--- src/main/java/com/epetnet/epetnet/config/MybatisPlusConf.java
+++ src/main/java/com/epetnet/epetnet/config/MybatisPlusConf.java
@@ -1,6 +1,7 @@
 package com.epetnet.epetnet.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,10 @@ public class MybatisPlusConf {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        //这是分页拦截器，后续要加乐观锁还要再加另外的拦截器
+        //分页拦截器
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        //乐观锁
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 }
